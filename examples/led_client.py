6678e64df2a1854d066ffd9c4353ad446b69f1ce
sfalexrog <sfalexrog@gmail.com>
2019-04-10 02:09:14 +0300

    examples: Simplify example

--- examples/led_client.py
+++ examples/led_client.py
@@ -1,29 +1,39 @@
 import rospy
-from ros_ws281x import srv, msg
+from ros_ws281x.srv import SetLeds
+from ros_ws281x.msg import LEDState, LEDStateArray
+from std_msgs.msg import ColorRGBA
 
 NUM_LEDS = 60
 
-setLeds = rospy.ServiceProxy("/led/set_leds", srv.SetLeds, persistent=True)
+setLeds = rospy.ServiceProxy("/led/set_leds", SetLeds, persistent=True)
+
+
+def fillStrip(red, green, blue):
+    ledMsg = LEDStateArray()
+    ledMsg.leds = []
+    for i in range(NUM_LEDS):
+        led = LEDState(i, ColorRGBA(red, green, blue, 0))
+        ledMsg.leds.append(led)
+    setLeds(ledMsg)
 
 
 def fillColor(color, duration):
     currentIntensity = 0.0
-    Led = msg.LEDState
-    ledMessage = msg.LEDStateArray()
+    ledMessage = LEDStateArray()
     while currentIntensity < 1.0:
         currentIntensity += 1.0 / 60.0
         ledMessage.leds = []
         for i in range(NUM_LEDS):
-            led = Led()
-            led.index = i
-            led.color.r = color['r'] * currentIntensity
-            led.color.g = color['g'] * currentIntensity
-            led.color.b = color['b'] * currentIntensity
+            led = LEDState(i, ColorRGBA(color['r'] * currentIntensity,
+                                        color['g'] * currentIntensity,
+                                        color['b'] * currentIntensity,
+                                        0))
             ledMessage.leds.append(led)
         setLeds(leds=ledMessage)
         rospy.sleep(duration / 60.0)
 
 
+fillStrip(0, 0, 0)
 fillColor({'r': 60.0, 'g': 0.0, 'b': 0.0}, 1.0)
 rospy.sleep(1.0)
 fillColor({'r': 0.0, 'g': 60.0, 'b': 0.0}, 1.0)
@@ -31,3 +41,4 @@ rospy.sleep(1.0)
 fillColor({'r': 0.0, 'g': 0.0, 'b': 60.0}, 1.0)
 rospy.sleep(1.0)
 fillColor({'r': 0.0, 'g': 0.0, 'b': 0.0}, 0.2)
+
