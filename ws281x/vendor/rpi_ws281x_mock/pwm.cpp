5b97faa69496193f692455c85ff1de0b855135c8
siddharthcb <61482903+siddharthcb@users.noreply.github.com>
2021-10-11 16:08:12 +0900

    Delete ws281x directory

--- ws281x/vendor/rpi_ws281x_mock/pwm.cpp
+++
@@ -1,8 +0,0 @@
-#include "pwm.h"
-#include <ros/console.h>
-
-int pwm_pin_alt(int chan, int pinnum)
-{
-    ROS_INFO_STREAM("pwm_pin_alt called (params: chan == " << chan << ", pinnum == " << pinnum << ")");
-    return 42;
-}
